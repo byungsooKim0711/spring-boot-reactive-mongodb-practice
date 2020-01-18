9b4a6e45b03a5d01b250e0382a243b903ee6a683
kimbs <vlakd0711@naver.com>
2020-01-18 22:19:19 +0900

    Add Index

--- src/main/java/org/kimbs/demo/document/Employee.java
+++ src/main/java/org/kimbs/demo/document/Employee.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
@@ -26,6 +27,7 @@ public class Employee {
     private String name;
 
     @Field("JOB")
+    @Indexed(name = "IDX_EMPLOYEE_JOB")
     private String job;
 
     @Field("MANAGER")
